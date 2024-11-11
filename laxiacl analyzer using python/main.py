295053f9c9d2669b384e661aa999352357311128
girvenA <164923429+girvenA@users.noreply.github.com>
2024-11-11 05:36:55 +0000

    updated lex

--- laxiacl analyzer using python/main.py
+++ laxiacl analyzer using python/main.py
@@ -1,61 +1,164 @@
-import ply.lex as lex
+# A simple lexical analyzer for PHP source code using PLY
 
-# list of token names
-tokens = [
-    'PRINT', 'ECHO',
-    'IF', 'ELSE', 'WHILE',
-    'ID', 'NUMBER', 'STRING',
-    'PLUS', 'MINUS', 'MODULO', 'DIVIDE',
-    'ASSIGN', 'SEMICOLON',
-    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
-    'NEWLINE', 'COMMENT'
+from ply import lex
+
+# List of PHP keywords
+keywords = {
+    'PRINT': 'PRINT',
+    'ECHO': 'ECHO',
+    'IF': 'IF',
+    'ELSE': 'ELSE',
+    'WHILE': 'WHILE',
+    'FOR': 'FOR',
+    'FUNCTION': 'FUNCTION',
+    'RETURN': 'RETURN',
+}
+
+# List of tokens
+tokens = list(keywords.values()) + [
+    'EQUALS', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+    'LPAREN', 'RPAREN', 'RCURLY', 'LCURLY', 'LT', 'LE', 'GT', 'GE', 'NE',
+    'CONCAT', 'COMMA', 'SEMI', 'NOT', 'MOD','IDENTIFIER', 'NUMBER', 'STRING',
+    'NEWLINE', 'COMMENT', 'PHP_OPEN', 'PHP_CLOSE',
 ]
 
-# regular expression rules for simple tokens
-t_PRINT = r'PRINT'
-t_ECHO = r'ECHO'
-t_IF = r'IF'
-t_ELSE = r'ELSE'
-t_WHILE = r'WHILE'
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_MODULO = r'%'
-t_DIVIDE = r'/'
-t_ASSIGN = r'='
-t_SEMICOLON = r';'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_COMMENT = r'//.*|/\*.*?\*/'
+# Ignore whitespace
+t_ignore = ' \t'
 
-# a regular expression rule
-def t_ID(t):
-    r'\$[a-zA-Z_][a-zA-z0-9_]*'
+# Token definitions
+def t_PHP_OPEN(t):
+    r'\<\?php'
+    return t
+
+def t_PHP_CLOSE(t):
+    r'\?\>'
+    return t
+
+def t_COMMENT(t):
+    r'//.*'
+    pass  # Ignore comments
+
+def t_PRINT(t):
+    r'PRINT'
+    return t
+
+def t_ECHO(t):
+    r'ECHO'
+    return t
+
+def t_IF(t):
+    r'IF'
+    return t
+
+def t_ELSE(t):
+    r'ELSE'
+    return t
+
+def t_WHILE(t):
+    r'WHILE'
+    return t
+
+def t_FOR(t):
+    r'FOR'
+    return t
+
+def t_FUNCTION(t):
+    r'FUNCTION'
+    return t
+
+def t_RETURN(t):
+    r'RETURN'
+    return t
+
+def t_IDENTIFIER(t):
+    r'\$[a-zA-Z_][a-zA-Z0-9_]*'  # Identifiers start with $ followed by letters or numbers
     return t
 
 def t_NUMBER(t):
     r'\d+'
-    t.value = int(t.value)
+    t.value = int(t.value)  # Convert to integer
     return t
 
 def t_STRING(t):
     r'"([^"\\]*(\\.[^"\\]*)*)"'
-    t.value = t.value[1:-1]
     return t
 
-# define a rule
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+t_EQUALS = r'='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'>='
+t_NE = r'<>'
+t_CONCAT = r'\.'  # Concatenation operator
+t_COMMA = r','
+t_SEMI = r';'
+t_NOT = r'!'
+t_MOD = r'%'
 
-# gnored white
-t_ignore = ' \t'
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    return t
 
-#error handler
 def t_error(t):
-    print("Illegal Character '{%t.value[0]}'")
+    print(f"Illegal character '{t.value[0]}' at line {t.lineno}")
     t.lexer.skip(1)
 
-# create lexer
-lexer = lex.lex()
+# Build the lexer
+lex.lex(debug=0)
+
+# Function to analyze input code
+def analyze_code(code):
+    lex.input(code)
+    while True:
+        token = lex.token()  # Get the next token
+        if not token:  # If there are no more tokens, break
+            break
+        print(token)
+
+# Example PHP code snippets to analyze
+
+# Example 1: Simple Print Statements
+php_code_1 = '''<?php //php 7.2.24
+PRINT "Hello, world! \\n"; ECHO "Welcome"; ?>'''
+
+# Example 2: Simple Arithmetic Expression
+php_code_2 = '''<?php
+$num1 = 10; $num2 = 20; $num3 = 30;
+$sum = $num1 + $num2 + $num3; $avg = $sum / 3;
+PRINT "Num1 is " . $num1 . "\\n"; PRINT "Num2 is " . $num2 . "\\n"; ?>'''
+
+# Example 3: Simple Conditional IF with Block
+php_code_3 = '''<?php //php 7.2.24
+$num1 = 10; $num2 = 20; IF ($num1 > $num2) {
+    $bignum = $num1; PRINT "Big Number is " . $bignum;
+} ELSE {
+    $bignum = $num2; PRINT "Big Number is " . $bignum;
+} ?>'''
+
+# Example 4: Simple Looping with Conditional IF Block
+php_code_4 = '''<?php //php 7.2.24
+PRINT "List of Odd Number 1-100:\\n"; $num = 1; WHILE ($num <= 100) {
+    IF (($num % 2) != 0) {
+        PRINT "" . $num . " ";
+    }
+    $num = $num + 1;
+} ?>'''
+
+# Analyze each example PHP code
+print("Analyzing PHP Code 1:")
+analyze_code(php_code_1)
+print("\nAnalyzing PHP Code 2:")
+analyze_code(php_code_2)
+print("\nAnalyzing PHP Code 3:")
+analyze_code(php_code_3)
+print("\nAnalyzing PHP Code 4:")
+analyze_code(php_code_4)
