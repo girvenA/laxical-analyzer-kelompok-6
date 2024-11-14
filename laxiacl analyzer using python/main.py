9a750fd0b0b0d6dc6dadd36b6de7c961af44e4e7
girvenA <164923429+girvenA@users.noreply.github.com>
2024-11-14 19:06:40 +0700

    Update main.py

--- laxiacl analyzer using python/main.py
+++ laxiacl analyzer using python/main.py
@@ -4,31 +4,59 @@
 # Example PHP code snippets to analyze
 
 # Example 1: Simple Print Statements
-code_1 = '''<?php //php 7.2.24
-PRINT "Hello, world! \\n"; ECHO "Welcome"; ?>'''
+code_1 = '''
+<?php //php 7.2.24
+    PRINT "Hello, world! \n";
+    ECHO "Welcome " 
+?>
+'''
 
 # Example 2: Simple Arithmetic Expression
-code_2 = '''<?php
-$num1 = 10; $num2 = 20; $num3 = 30;
-$sum = $num1 + $num2 + $num3; $avg = $sum / 3;
-PRINT "Num1 is " . $num1 . "\\n"; PRINT "Num2 is " . $num2 . "\\n"; ?>'''
+code_2 = '''
+<?php
+    $num1 = 10; 
+    $num2 = 20;
+    $num3 = 30;
+    $sum = $num1 + $num2 + $num3;
+    $avg = $sum/3;
+    PRINT "Num1 is " . $num1 ."\n";
+    PRINT "Num2 is " . $num2 ."\n";
+    PRINT "Num3 is " . $num3 ."\n";
+    PRINT "Sum 3 numbers is " . $sum ."\n";
+    PRINT "Average is " . $avg;
+?>
+'''
 
 # Example 3: Simple Conditional IF with Block
-code_3 = '''<?php //php 7.2.24
-$num1 = 10; $num2 = 20; IF ($num1 > $num2) {
-    $bignum = $num1; PRINT "Big Number is " . $bignum;
-} ELSE {
-    $bignum = $num2; PRINT "Big Number is " . $bignum;
-} ?>'''
+code_3 = '''
+<?php //php 7.2.24
+    $num1 = 10; 
+    $num2 = 20;     
+    IF ($num1 > $num2) {
+        $bignum = $num1; 
+        PRINT "Big Number is " . $bignum;
+    } 
+    ELSE {
+        $bignum = $num2; 
+        PRINT "Big Number is " . $bignum;
+    } 
+?>
+'''
 
 # Example 4: Simple Looping with Conditional IF Block
-code_4 = '''<?php //php 7.2.24
-PRINT "List of Odd Number 1-100:\\n"; $num = 1; WHILE ($num <= 100) {
-    IF (($num % 2) != 0) {
-        PRINT "" . $num . " ";
-    }
-    $num = $num + 1;
-} ?>'''
+code_4 = '''
+<?php //php 7.2.24
+    PRINT "List of Odd Number 1-100:\n";
+    PRINT "\n":
+    $num = 1; 
+    WHILE ($num <= 100) {
+        IF (($num % 2) != 0) {
+            $oddnum=$num
+            PRINT "" . $num . " "; }
+        $num=$num + 1;
+    } 
+?>
+'''
 
 
 #import lex from python lex yacc
